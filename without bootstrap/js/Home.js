dcaed936d499e0040e07874b682b8bdce0948849
Nawar <nwaralrwdan@gmail.com>
2021-06-29 15:39:15 +0300

    pop-up added

--- without bootstrap/js/Home.js
+++ without bootstrap/js/Home.js
@@ -1,1 +1,20 @@
-document.getElementById("demo").innerHTML = "Hello JavaScript";
+document.getElementById("title").innerHTML = "Hello JavaScript";
+
+document.getElementById("button").addEventListener("click",
+function() {
+  document.querySelector('.bg-model').style.display='flex';
+});
+document.querySelector('.close').addEventListener('click',
+function() {
+  document.querySelector('.bg-model').style.display='none';
+});
+
+document.querySelector('.yes-btn').addEventListener('click',
+function() {
+  document.querySelector('.bg-model').style.display='none';
+});
+
+document.querySelector('.back-btn').addEventListener('click',
+function() {
+  document.querySelector('.bg-model').style.display='none';
+});
